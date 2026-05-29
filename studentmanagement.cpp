01544a769fa5a26ad685d1bd1ed0606c1c6d8eaa
Heck_ClearVision_ <abdxllahmazharx@gmail.com>
2026-05-29 22:39:26 +0500

    Update studentmanagement.cpp

--- studentmanagement.cpp
+++ studentmanagement.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+string gender[100];
 string students[100];
 bool deleted[100] = {false};
 int count = 0;
@@ -13,7 +14,8 @@ int main(){
         cout<<"2. View Students"<<endl;
         cout<<"3. Search Students"<<endl;
         cout<<"4. Delete Students"<<endl;
-        cout<<"5. Exit"<<endl;
+        cout<<"5. View Student Gender"<<endl;
+        cout<<"6. Exit"<<endl;
         cin>>choice;
         cin.clear();
         cin.ignore(10000, '\n');
@@ -21,8 +23,11 @@ int main(){
         switch (choice)
         {
         case 1:
+
             cout<<"Enter Name of the Student: "<<endl;
             cin>>students[count];
+            cout<<"Enter Gender of the Student (M/F): "<<endl;
+            cin>>gender[count];
             deleted[count] = false;
             cout<<"Student Added!";
             
@@ -40,6 +45,7 @@ int main(){
                     if(deleted[i] == false){
                         cout<< i+1<<". "
                             << students[i]<<endl;
+                        cout<<"Gender: "<<gender[i]<<endl;
                     }
                 }
             }
@@ -61,7 +67,7 @@ int main(){
                 cout<<"Student not found!"<<endl;
             }
             break;
-        }
+        } 
 
         case 4: {
             cout<<"Enter name of the Student to delete: "<<endl;
@@ -86,7 +92,24 @@ int main(){
         }
 
         case 5:
-            cout<<"Exiting....";
+            cout<<"===================="<<endl;
+            cout<<"Student Gender List:"<<endl;
+            if(count == 0){
+                cout<<"There are 0 Students!";
+            }
+            else{
+                for(int i = 0; i < count; i++){
+                    if(deleted[i] == false){
+                        cout<< i+1<<". "
+                            << students[i]<<endl;
+                        cout<<"Gender: "<<gender[i]<<endl;
+                    }
+                }
+            }
+            break;
+
+            case 6:
+            cout<<"Exiting the program. Goodbye!"<<endl;
             break;
 
         default:
